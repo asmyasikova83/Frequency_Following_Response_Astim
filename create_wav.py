2ee5a3fb7ea5d40933a0adc498d2f3d3e5205cda
asmyasnikova83 <asmyasnikova83@gmail.com>
2026-05-04 12:03:35 +0300

    parameter ranges in crreate.wav

--- create_wav.py
+++ create_wav.py
@@ -204,17 +204,35 @@ def main():
                             choices=['repeated_da', 'multiple_sin'],
                             help='Function to generate stimuli: repeated_da или multiple_sin')
     parser.add_argument('--dirname', type=str, default='M:\\DB Temp\\physionet.org\\files\\ffr_astim',
-                        help='Директория для сохранения  wav файла (по умолчанию: M:\\DB Temp\\physionet.org\\files\\ffr_astim)')
-    parser.add_argument('--F', type=int, nargs='+', default=[], help='Частоты (Гц)')
+                        help='Directory for saving WAV file (default: M:\\DB Temp\\physionet.org\\files\\ffr_astim)')
+    parser.add_argument('--F', type=int, nargs='+', default=[], help='Frequencies (Hz).\
+    Default number of frequencies is 0 (no frequencies). Maximum number of frequencies is 5')
     parser.add_argument('--TS', type=float, required=True, help='Length of the stimulus (ms)')
     parser.add_argument('--TP', type=float, required=True, help='Interstimulus interval (ms)')
     parser.add_argument('--N', type=int, required=True, help='Number of repetitions of the stimulus')
-    parser.add_argument('--SR', type=int, default=44100, help='Sampling rate default: 44100 Hz')
-    parser.add_argument('--INV', type=int, required=True, help='Add an inverted (polar) stimuli')
+    parser.add_argument('--SR', type=int, default=44100, help='Sampling rate default: 44100 Hz (fixed: 44100 Hz)')
+    parser.add_argument('--INV', type=int, required=True,
+                            choices=[0, 1], help='Add an inverted (polar) stimuli')
     """
     Example call:  python create_wav.py  --function multiple_sin --F 440 880 --TS 100 --TP 100 --N 1 --INV 0
     """
     args = parser.parse_args()
+    F = np.array(args.F)
+
+    # Validation of parameter ranges
+    if F.shape[0] > 5:
+        parser.error("argument --F: value must be in range 1 - 5, got {}".format(len(args.F)))
+    for f in F:
+        if not 1 < f < 1000:
+            parser.error(f'argument --F: values must be in range 1 - 1000 Hz, got {f}')
+    if not (50 <= args.TS <= 750):
+        parser.error('argument --TS: value must be in range 50–750 ms')
+    if not (100 <= args.TP <= 750):
+        parser.error('argument --TP: value must be in range 100–750 ms')
+    if not (2 <= args.N <= 10000):
+        parser.error('argument --N: value must be in range 2 to 10000')
+    if args.SR != 44100:
+        parser.error("Sampling rate must be exactly 44100 Hz, got {}".format(args.SR))
 
     if args.function == 'repeated_da':
         create_repeated_da_syllable_wav(
