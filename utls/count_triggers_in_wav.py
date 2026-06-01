73a8823e288d4564fab52867650b5cc29c16d431
asmyasnikova83 <asmyasnikova83@gmail.com>
2026-06-01 09:43:26 +0300

    report time jitter

--- utls/count_triggers_in_wav.py
+++ utls/count_triggers_in_wav.py
@@ -5,7 +5,9 @@ from scipy.io import wavfile
 
 
 #fname  = r'\\MCSSERVER\DB Temp\physionet.org\files\ffr_astim\Da_syll_TS250.0ms_TP200.0ms_N2000_INV1.wav'
-fname  = r'\\MCSSERVER\DB Temp\physionet.org\files\ffr_astim\Da_syll_TS250.0ms_TP200.0ms_N3000_INV1.wav'
+#fname  = r'\\MCSSERVER\DB Temp\physionet.org\files\ffr_astim\Da_syll_TS250.0ms_TP200.0ms_N3000_INV1.wav'
+fname  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\stim\Da_syll_TS250.0ms_TP200.0ms_N4000_Amplitude_INV1.wav'
+
 #fname  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\stim\DA_syll_TS250ms_N4000_A100.0%_INV1.wav'
 #fname  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\stim\short\G_note_TS100ms_TP100ms_N4000_A100%_INV1.wav'
 #fname  =  r'\\MCSSERVER\DB Temp\physionet.org\FFR\stim\short\DA_syll_TS90ms_N4000_A100%_INV1.wav'
@@ -13,6 +15,7 @@ _SILENCE = 1
 max_int16 = np.iinfo(np.int16).max
 min_int16 = np.iinfo(np.int16).min
 
+print(f'Processing {fname}....')
 ##################################count_triggers#############################
 # 100
 seq_6low = [max_int16, min_int16, min_int16, max_int16, min_int16, max_int16]
@@ -35,7 +38,7 @@ def count_wav_triggers_optimized(data, sequence):
     matches = np.where(corr == target_corr)[0]
     return len(matches), matches.tolist()
 
-# Использование оптимизированной версии
+
 count_opt_6low, indices_opt_6low = count_wav_triggers_optimized(stim[:, 1], seq_6low)
 count_opt_7low, indices_opt_7low = count_wav_triggers_optimized(stim[:, 1], seq_7low)
 
@@ -47,57 +50,3 @@ print(f"7low - {count_opt_7low} вхождений")
 
 print(f"6high - {count_opt_6high} вхождений")
 print(f"7high - {count_opt_7high} вхождений")
-"""
-print(f"6low - {indices_opt_6low} ")
-print(f"7low - {indices_opt_7low} ")
-print(f"6high - {indices_opt_6high} ")
-print(f"7high - {indices_opt_7high} ")
-"""
-
-def compute_mean_std(intervals, stim, fs):
-    """
-    Computes stat for intervals
-    """
-    print('-------intervals', intervals[:10])
-    if stim:
-        intervals_filtered = intervals[intervals == 11018]
-        print('len stims ', len(intervals_filtered))
-    else:
-        # Exclude stimuli if not stim (pause)
-        intervals_filtered = intervals[intervals != 11018]
-        print('len pauses ', len(intervals_filtered))
-    # Convert into seconds
-    intervals_seconds = intervals_filtered / fs
-    print('intervals_seconds ', intervals_seconds[:10])
-
-    # Считаем среднее и стандартное отклонение
-    mean_interval = round(np.mean(intervals_seconds), 3)
-    std_interval = round(np.std(intervals_seconds, ddof=0), 3)  # ddof=1 for unbiased estimate
-    return mean_interval, std_interval
-
-#########################compute_isi###########################################
-def compute_interval_stat(indices_opt_6low, indices_opt_6high, indices_opt_7low, indices_opt_7high, fs=44100):
-    """
-    Computes statistics for stim, for pause
-    """
-
-    all_indices = np.concatenate([
-        indices_opt_6low,
-        indices_opt_6high,
-        indices_opt_7low,
-        indices_opt_7high,
-    ])
-    all_indices_sorted = np.sort(all_indices)
-
-    # Compute diff betw adjacent indices
-    intervals = np.diff(all_indices_sorted)
-
-    # Compute stat for stim, for pause
-    stim = True
-    mean_stim, std_stim = compute_mean_std(intervals, stim, fs)
-    stim = False
-    mean_pause, std_pause = compute_mean_std(intervals, stim, fs)
-    return mean_stim, std_stim, mean_pause, std_pause
-
-mean_stim, std_stim, mean_pause, std_pause = compute_interval_stat(indices_opt_6low, indices_opt_6high, indices_opt_7low, indices_opt_7high, fs=44100)
-print(' Mean stim interval: ', mean_stim, '\n', 'Std stim interval: ',  std_stim, '\n', 'Mean pause interval: ', mean_pause, '\n', 'Std pause interval: ',  std_pause)
--- utls/report_triggers_pdf.py
+++ utls/report_triggers_pdf.py
@@ -40,6 +40,7 @@ def compute_mean_std(intervals, bdf,  stim, fs, fname_bdf, fs_bdf, fname_wav):
     """
     Computes stat for intervals
     """
+    #Most frequent value in intervals is stim
     stimulus= pd.Series(intervals).value_counts().index[:2]
     print(f"Most common value: {stimulus[0] } {stimulus[1] } ")
 
@@ -57,7 +58,6 @@ def compute_mean_std(intervals, bdf,  stim, fs, fname_bdf, fs_bdf, fname_wav):
     mask = np.isin(intervals, test_intervals)
     if stim:
         intervals_filtered = intervals[mask]
-        # Случай: только стимул — получаем индексы и сохраняем в CSV
         boolean_stim = np.where(mask)[0]
         fname_indices_stim = os.path.join(output_path, f'boolean_stim_bdf_{bdf}.csv')
         np.savetxt(fname_indices_stim, boolean_stim, fmt='%d', delimiter=',')
@@ -75,10 +75,29 @@ def compute_mean_std(intervals, bdf,  stim, fs, fname_bdf, fs_bdf, fname_wav):
 
     return mean_interval, std_interval
 
-def plot_deviations(ax, intervals_wav_s_cum_with_start, intervals_bdf_s_cum_with_start):
-    m = min(len(intervals_wav_s_cum_with_start), len(intervals_bdf_s_cum_with_start))
-    diff_intervals = 1000 * (intervals_wav_s_cum_with_start[:m] - intervals_bdf_s_cum_with_start[:m])
-    data = np.array(diff_intervals)
+def plot_deviations(ax, intervals_wav_s_cum_with_start, intervals_bdf_s_with_start):
+    m = min(len(intervals_wav_s_cum_with_start), len(intervals_bdf_s_with_start))
+
+    diff_intervals = 1000 * (intervals_wav_s_cum_with_start[:m] - intervals_bdf_s_with_start[:m])
+    data = np.round(diff_intervals, decimals=3)
+    print('diff_intervals', diff_intervals)
+
+    print('intervals_wav_s_cum_with_start',  intervals_wav_s_cum_with_start[710:730])
+    print('intervals_bdf_s_with_start', intervals_bdf_s_with_start[710:730])
+
+    print('--------------------time jitter min, ms', data.min())
+    print('--------------------time jitter max, ms', data.max())
+
+    # Находим индексы, где разница достигает минимума и максимума
+    idx_min = np.argmin(data)
+    idx_max = np.argmax(data)
+
+    print('Индекс, где разница минимальна:', idx_min)
+    print('Индекс, где разница максимальна:', idx_max)
+    print('Значение разницы в минимуме:', data[idx_min])
+    print('Значение разницы в максимуме:', data[idx_max])
+
+    diff_big = len(np.where(abs(data) > 1)[0])
 
     mpl.rcParams['patch.linewidth'] = 2
     # Общая гистограмма (Counts)
@@ -97,7 +116,6 @@ def plot_deviations(ax, intervals_wav_s_cum_with_start, intervals_bdf_s_cum_with
     x_grid = np.linspace(data.min(), data.max(), 2000)
     y_kde = kde(x_grid)
 
-
     peaks, _ = find_peaks(
         y_kde,
         distance=50
@@ -105,7 +123,7 @@ def plot_deviations(ax, intervals_wav_s_cum_with_start, intervals_bdf_s_cum_with
 
     colors = plt.cm.Set2.colors
 
-    # Размер локального окна вокруг моды
+    # Window around the mode
     window_ratio = 0.08
     window = (data.max() - data.min()) * window_ratio
 
@@ -143,9 +161,17 @@ def plot_deviations(ax, intervals_wav_s_cum_with_start, intervals_bdf_s_cum_with
             ha='right'
         )
 
-    # Среднее значение
-    mean_diff = np.mean(data)
-    mean_diff = round(mean_diff)
+    ax.text(
+        0.05, 0.95,  # upper left corner
+        f' Count Time jitters > 1 ms: \n {round(diff_big / 4000 , 2)} %',
+        color='red',
+        fontsize=14,
+        fontweight='bold',
+        ha='left',  # allignment on the left
+        va='top',
+        transform=ax.transAxes,
+        bbox=dict(boxstyle="round,pad=0.3", facecolor="wheat", alpha=0.8)
+    )
 
     title = 'Distribution of deviations of bdf from wav triggers, ms'
     ax.set_title(title, fontsize=16, fontweight='bold', pad=20)
@@ -162,31 +188,31 @@ def plot_deviations(ax, intervals_wav_s_cum_with_start, intervals_bdf_s_cum_with
     return title
 
 def prepare_intervals(intervals_bdf_s, intervals_wav_s):
-
-    intervals_bdf_s_cum = np.cumsum(intervals_bdf_s)
-    intervals_bdf_s_cum_with_start = np.concatenate([[0], intervals_bdf_s_cum])
+    intervals_bdf_with_start = np.concatenate([[0], intervals_bdf_s])
     fname_stim_bdf = os.path.join(output_path, 'boolean_stim_bdf_True.csv')
     stim_bdf = np.loadtxt(fname_stim_bdf, dtype=int)
 
     intervals_wav_s_cum = np.cumsum(intervals_wav_s)
+    intervals_wav_s_with_start = np.concatenate([[0], intervals_wav_s])
     intervals_wav_s_cum_with_start = np.concatenate([[0], intervals_wav_s_cum])
-
     fname_stim_wav = os.path.join(output_path, 'boolean_stim_bdf_False.csv')
     stim_wav = np.loadtxt(fname_stim_wav, dtype=int)
     stim_wav = np.array(stim_wav)
 
-    return intervals_wav_s_cum_with_start, intervals_bdf_s_cum_with_start, stim_wav, stim_bdf
+    return intervals_wav_s_cum_with_start, intervals_wav_s_with_start, intervals_bdf_with_start, stim_wav, stim_bdf
 
-def plot_triggers(ax2, ax3, ax4, intervals_wav_s_cum_with_start, intervals_bdf_s_cum_with_start, stim_wav,
-                  stim_bdf, filename_bdf, filename_wav):
+def plot_triggers(ax2, ax3, ax4, events_bdf_s, intervals_wav_s_cum_with_start,
+                  stim_wav, stim_bdf, filename_bdf, filename_wav):
+    events_bdf_s_corr = events_bdf_s[:] - events_bdf_s.min()
+
     ax2.vlines(
-        intervals_wav_s_cum_with_start[:trim_interval],
-        ymin=0,
-        ymax=0.4,
-        colors='red',
-        linewidth=3,
-        alpha=0.6,
-        label=f'WAV {filename_wav}'  # убираем fontsize из этой строки
+    intervals_wav_s_cum_with_start[:trim_interval],
+    ymin=0,
+    ymax=0.4,
+    colors='red',
+    linewidth=3,
+    alpha=0.6,
+    label=f'WAV {filename_wav}'  # убираем fontsize из этой строки
     )
 
     ax2.legend(loc='upper left', fontsize=16)
@@ -195,15 +221,14 @@ def plot_triggers(ax2, ax3, ax4, intervals_wav_s_cum_with_start, intervals_bdf_s
 
     for xmin_i in stim_wav_trim:
         ax2.hlines(
-            y=0.4,  # уровень линии соответствует значению stim_bdf
-            xmin=intervals_wav_s_cum_with_start[xmin_i],
-            xmax=intervals_wav_s_cum_with_start[xmin_i + 1],
-            colors='red',
-            linewidth=3,
-            alpha=0.6
-        )
+        y=0.4,
+        xmin=intervals_wav_s_cum_with_start[xmin_i],
+        xmax=intervals_wav_s_cum_with_start[xmin_i + 1],
+        colors='red',
+        linewidth=3,
+        alpha=0.6
+    )
 
-    # Настройка верхнего подграфика
     ax2.set_ylim(0, 0.7)
     ax2.legend(loc='upper left', fontsize=16)
     ax2.grid(axis='x', alpha=0.3)
@@ -211,20 +236,21 @@ def plot_triggers(ax2, ax3, ax4, intervals_wav_s_cum_with_start, intervals_bdf_s
     ax2.set_yticklabels([])
     ax2.set_xticklabels([])
 
-    ax3.vlines(intervals_bdf_s_cum_with_start[:trim_interval], ymin=0, ymax=0.7,
-               colors='blue', linewidth=2, alpha=0.4, label=f'BDF {filename_bdf}')
+    ax3.vlines(events_bdf_s_corr[:trim_interval], ymin=0, ymax=0.7,
+           colors='blue', linewidth=2, alpha=0.4, label=f'BDF {filename_bdf}')
 
     ax3.legend(loc='upper left', fontsize=16)
     stim_bdf_trim = stim_bdf[stim_bdf < trim_interval - 1]
+
     for xmin_i in stim_bdf_trim:
         ax3.hlines(
-            y=0.7,  # уровень линии соответствует значению stim_bdf
-            xmin=intervals_bdf_s_cum_with_start[xmin_i],
-            xmax=intervals_bdf_s_cum_with_start[xmin_i + 1],
-            colors='blue',
-            linewidth=2,
-            alpha=0.6
-        )
+        y=0.7,  # уровень линии соответствует значению stim_bdf
+        xmin=events_bdf_s_corr[xmin_i],
+        xmax=events_bdf_s_corr[xmin_i + 1],
+        colors='blue',
+        linewidth=2,
+        alpha=0.6
+    )
 
     ax3.set_ylim(0, 1.0)
     ax3.set_xticklabels([])
@@ -236,44 +262,49 @@ def plot_triggers(ax2, ax3, ax4, intervals_wav_s_cum_with_start, intervals_bdf_s
     ax3.tick_params(axis='y', labelsize=16)
 
     ax4.vlines(
-        intervals_wav_s_cum_with_start[:trim_interval],
-        ymin=0,
-        ymax=0.4,
-        colors='red',
-        linewidth=3,
-        alpha=0.6,
-        label=f'WAV {filename_wav}'
-    )
-    # BDF данные — синие
+    intervals_wav_s_cum_with_start[:trim_interval],
+    ymin=0,
+    ymax=0.4,
+    colors='red',
+    linewidth=3,
+    alpha=0.6,
+    label=f'WAV {filename_wav}'
+        )
     ax4.vlines(
-        intervals_bdf_s_cum_with_start[:trim_interval],
-        ymin=0,
-        ymax=0.7,
-        colors='blue',
-        linewidth=2,
-        alpha=0.4,
-        label=f'BDF {filename_bdf}'
+    events_bdf_s_corr[:trim_interval],
+    ymin=0,
+    ymax=0.7,
+    colors='blue',
+    linewidth=2,
+    alpha=0.4,
+    label=f'BDF {filename_bdf}'
     )
 
     for xmin_i in stim_wav_trim:
         ax4.hlines(
-            y=0.4,
-            xmin=intervals_wav_s_cum_with_start[xmin_i],
-            xmax=intervals_wav_s_cum_with_start[xmin_i + 1],
-            colors='red',
-            linewidth=3,
-            alpha=0.6
-        )
+        y=0.4,
+        xmin=intervals_wav_s_cum_with_start[xmin_i],
+        xmax=intervals_wav_s_cum_with_start[xmin_i + 1],
+        colors='red',
+        linewidth=3,
+        alpha=0.6
+    )
     for xmin_i in stim_bdf_trim:
         ax4.hlines(
-            y=0.7,
-            xmin=intervals_bdf_s_cum_with_start[xmin_i],
-            xmax=intervals_bdf_s_cum_with_start[xmin_i + 1],
-            colors='blue',
-            linewidth=2,
-            alpha=0.6
-        )
+        y=0.7,
+        xmin=events_bdf_s_corr[xmin_i],
+        xmax=events_bdf_s_corr[xmin_i + 1],
+        colors='blue',
+        linewidth=2,
+        alpha=0.6
+    )
 
+    # Общие настройки для ax4
+    ax4.set_ylim(0, 1.0)
+    ax4.legend(loc='upper left', fontsize=16)
+    ax4.grid(axis='x', alpha=0.3)
+    ax4.tick_params(axis='x', labelsize=16, rotation=45)
+    ax4.tick_params(axis='y', labelsize=16)
     ax4.set_ylim(0, 1.4)
     ax4.set_xlabel('Time (s)', fontsize=18)
     ax4.legend(loc='upper left', fontsize=16)
@@ -282,14 +313,15 @@ def plot_triggers(ax2, ax3, ax4, intervals_wav_s_cum_with_start, intervals_bdf_s
     ax4.set_yticklabels([])  # убираем метки оси Y
 
 
-def plot_intervals(dummy, intervals_bdf_s: object, intervals_wav_s: object, filename_bdf, filename_wav) -> None:
+
+def plot_intervals(dummy,  events_bdf_tm, intervals_bdf_s, intervals_wav_s, filename_bdf, filename_wav) -> None:
+
     """
     Plots intervals_bdf_ms, intervals_wav_ms on one scale
     """
 
-    (intervals_wav_s_cum_with_start, intervals_bdf_s_cum_with_start,
-     stim_wav, stim_bdf) = prepare_intervals(intervals_bdf_s, intervals_wav_s)
-
+    (intervals_wav_s_cum_with_start, intervals_wav_s_with_start,
+     intervals_bdf_s_with_start, stim_wav, stim_bdf) = prepare_intervals(intervals_bdf_s, intervals_wav_s)
 
     fig = plt.figure(figsize=(40, 12))
 
@@ -305,21 +337,20 @@ def plot_intervals(dummy, intervals_bdf_s: object, intervals_wav_s: object, file
     ax_stats = fig.add_subplot(gs[2, 0])
     ax_stats.axis('off')  # скрываем оси — они не нужны для текста
 
-    title = plot_deviations(ax1, intervals_wav_s_cum_with_start, intervals_bdf_s_cum_with_start)
+    title = plot_deviations(ax1, intervals_wav_s_with_start, intervals_bdf_s_with_start)
     ax1.set_title(title, fontsize=16)
 
-    plot_triggers(ax2, ax3, ax4, intervals_wav_s_cum_with_start, intervals_bdf_s_cum_with_start,
-                  stim_wav, stim_bdf, filename_bdf, filename_wav)
+    plot_triggers(ax2, ax3, ax4, events_bdf_tm, intervals_wav_s_cum_with_start,
+                  stim_wav, stim_bdf, fname_bdf, fname_wav)
     ax2.set_title('Timing of triggers', fontsize=18)
 
-    # Собираем статистику событий
+    # Statistics
     if dummy:
         event_counts = {
         'In\\6': extract_n_events(events_bdf, event_dict, label='In\\6'),
         'In/6': extract_n_events(events_bdf, event_dict, label='In/6'),
         'In\\7': extract_n_events(events_bdf, event_dict, label='In\\7'),
         'In/7': extract_n_events(events_bdf, event_dict, label='In/7'),
-        #'N wav': filename_wav.split('_')[-3]
         'N wav': filename_wav.split('_')[-3]
         }
     else:
@@ -337,38 +368,38 @@ def plot_intervals(dummy, intervals_bdf_s: object, intervals_wav_s: object, file
     for label, count in event_counts.items():
         text_str += f"{label}: {count}\n"
 
-    # Добавляем текст на новую ось в первом столбце
     ax_stats.text(0.5, 0.5, text_str,
                   transform=ax_stats.transAxes,
                   fontsize=14,
-                  ha='center',  # горизонтальное выравнивание по центру
-                  va='center',  # вертикальное выравнивание по центру
+                  ha='center',
+                  va='center',
                   bbox=dict(boxstyle="round,pad=1", facecolor="lightblue", alpha=0.7),
                   fontfamily='monospace')
 
-    # Сохранение в высоком разрешении
     filename_bdf = filename_bdf.split('.')[0] + filename_wav.split('_')[-3] + '.pdf'
     full_path_pdf = os.path.join(output_path, filename_bdf)
 
     fig.suptitle(filename_bdf, fontsize=18, fontweight='bold', y=0.98)
     fig.savefig(full_path_pdf, format='pdf', dpi=600, bbox_inches='tight', pad_inches=0.2)
+    os.startfile(full_path_pdf)
 
 
 def compute_interval_stat(dummy, events_wav, events_bdf, stat, fs_bdf, fs_wav, fname_bdf, fname_wav):
+
     """
     Computes statistics for stim, for pause
     """
 
     # Compute diff betw adjacent indices
     intervals_bdf = np.diff(events_bdf[:, 0])
-    print('-----events_bdf', events_bdf)
+
     intervals_bdf_s = intervals_bdf / fs_bdf
 
     intervals_wav = np.diff(events_wav)
 
     intervals_wav_s = intervals_wav / fs_wav
 
-    plot_intervals(dummy, intervals_bdf_s, intervals_wav_s, fname_bdf, fname_wav)
+    plot_intervals(dummy,  events_bdf[:, 0] / fs_bdf,  intervals_bdf_s, intervals_wav_s, fname_bdf, fname_wav)
 
     if stat:
         # Compute stat for stim, for pause
@@ -387,20 +418,23 @@ def compute_interval_stat(dummy, events_wav, events_bdf, stat, fs_bdf, fs_wav, f
         print('mean_stim', mean_stim, 'std_stim', std_stim, 'mean_pause', mean_pause, 'std_pause', std_pause)
 
 
-    plot_intervals(dummy, intervals_bdf_s, intervals_wav_s, fname_bdf, fname_wav)
-
 
 #fname_bdf  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\data\non_filt\preamplifier\ffr_da_N4000_non_filtS0preamplifier.bdf'
 #fname_bdf  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\data\non_filt\preamplifier\ffr_da_N4000_non_filtS1preamplifier.bdf'
 #fname_bdf  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\data\ffr_da_N4000_S0_step2.bdf' #events_bdf[1:, :]
 #fname_bdf  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\data\ffr_da_N4000_S0_step1.bdf' #events_bdf[1:, :]
-fname_bdf  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\data\non_filt\preamplifier\ffr_da_N4000_non_filtS1preamplifiershort.bdf'
+#fname_bdf  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\data\test_triggers\test_1.bdf'
+#fname_bdf  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\data\test_triggers\test_AIMP_cash250MB_no_anticlipping.bdf'
+fname_bdf  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\data\test_triggers\test_PlayPcmWin_WASAPI_Exclusive_long.bdf'
+#fname_bdf  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\data\test_triggers\test_generator.bdf'
+#fname_bdf  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\data\non_filt\preamplifier\ffr_da_N4000_non_filtS1preamplifiershort.bdf'
 #fname_bdf  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\data\non_filt\preamplifier\ffr_da_N4000_non_filtS1preamplifiershortG.bdf'
 #fname_bdf  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\data\non_filt\dummy\ffr_da_N4000_dummynon_filt.bdf' #events_bdf[1:, :-2]
 #fname_bdf  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\data\non_filt\dummy\preamplifier\ffr_da_N4000_dummynon_filtpreamplifier.bdf' #events_bdf[1:, :-2]
 #fname_bdf  = r'\\mcsserver\public\Rsergey\ASTIM\NeoRec_2026-05-08_11-12-53.bdf'
+fname_wav  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\stim\Da_syll_TS250.0ms_TP200.0ms_N4000_Amplitude_INV1.wav'
 #fname_wav  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\stim\DA_syll_TS250ms_N4000_A100.0%_INV1.wav'
-fname_wav  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\stim\short\DA_syll_TS90ms_N4000_A100%_INV1.wav'
+#fname_wav  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\stim\short\DA_syll_TS90ms_N4000_A100%_INV1.wav'
 #fname_wav  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\stim\short\G_note_TS100ms_TP100ms_N4000_A100%_INV1.wav'
 # with a false cycle
 #fname_wav  = r'\\MCSSERVER\DB Temp\physionet.org\FFR\stim\Da_syll_TS250.0ms_TP200.0ms_N2000_INV1.wav'
@@ -433,9 +467,6 @@ raw = mne.io.read_raw_bdf(
 
 events_bdf, event_dict = mne.events_from_annotations(raw)
 fs_bdf = int(raw.info.get('sfreq'))
-print('events_bdf', events_bdf)
-print('event_dict', event_dict)
-
 #count trigs in wav/bdf
 
 fs_wav, stim = wavfile.read(fname_wav)
